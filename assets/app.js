3503189d343ed2451d2f80df840b786546d85c93
jbng <johann.bertrand.ng@gmail.com>
2024-05-05 01:02:56 +0400

    fix: modal exchange

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,3 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-
-import 'flowbite';
-
-import { Trade } from "./pages/Trade"
 import "./styles/app.css"
 import 'flowbite';
 import 'boxicons';
--- assets/components/ui/ModalAccept/ModalAccept.js
+++ assets/components/ui/ModalAccept/ModalAccept.js
@@ -1,7 +1,7 @@
 import React from "react"
 import clsx from "clsx"
 
-const ModalExchangeButton = ({ toggleModal, setToggleModal }) => {
+const ModalAcceptButton = ({ toggleModal, setToggleModal }) => {
   return (
     <div className="flex justify-center m-5">
       <button
@@ -12,7 +12,7 @@ const ModalExchangeButton = ({ toggleModal, setToggleModal }) => {
         type="button"
         onClick={() => setToggleModal(!toggleModal)}
       >
-        Echange
+        Accepter
       </button>
     </div>
   )
@@ -77,14 +77,14 @@ const ModalFooter = () => {
               d="M15.041 13.862A4.999 4.999 0 0 1 17 17.831V21M7 3v3.169a5 5 0 0 0 1.891 3.916M17 3v3.169a5 5 0 0 1-2.428 4.288l-5.144 3.086A5 5 0 0 0 7 17.831V21M7 5h10M7.399 8h9.252M8 16h8.652M7 19h10"
             />
           </svg>
-          Echanger
+          Accepter
         </button>
       </div>
     </div>
   )
 }
 
-const ModalExchange = ({
+const ModalAccept = ({
   superPowerName,
   description,
   category,
@@ -95,7 +95,7 @@ const ModalExchange = ({
     "overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full"
   return (
     <div
-      id="ModalExchange"
+      id="ModalAccept"
       tabindex="-1"
       aria-hidden={!toggleModal}
       aria-modal={toggleModal}
@@ -134,4 +134,4 @@ const ModalExchange = ({
   )
 }
 
-export { ModalExchangeButton, ModalExchange }
+export { ModalAcceptButton, ModalAccept }
