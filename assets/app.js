91e1455400157da98059d5d6fcb7e5823d6b0830
Thomas Epicharme <tepicharme.dev@protonmail.com>
2024-05-04 20:06:00 +0400

    uppercase trade

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-import { Trade } from "./pages/trade"
+import { Trade } from "./pages/Trade"
 import "./styles/app.css"
 
 const root = ReactDOM.createRoot(document.getElementById("app-root"))
