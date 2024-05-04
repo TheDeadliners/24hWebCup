baaf4b13639e4952192a962e6e62696f9c3fe464
AllanDev97 <134329913+AllanDev97@users.noreply.github.com>
2024-05-04 17:50:28 +0400

    feat/register (#1)

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,8 +2,16 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -23,10 +31,45 @@ class SecurityController extends AbstractController
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
+    #[Route(path: '/creer-un-compte', name: 'app_user_new')]
+    public function newUser(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        // Vérifier si le formulaire a été soumis et que les champs requis sont remplis
+        if ($request->isMethod('POST')) {
+            $email = $request->request->get('email');
+            $password = $request->request->get('password');
+
+            if (!$email || !$password) {
+                return $this->redirectToRoute('app_user_new');
+            }
+            // Vérifier si l'utilisateur existe déjà
+            $existingUser = $entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
+            if ($existingUser) {
+                $this->addFlash('fail','Un compte existe déjà');
+                return $this->redirectToRoute('app_user_new');
+            }
+            $user = new User();
+            $email = $request->request->get('email');
+            $password = $request->request->get('password');
+            $hashpassword = $passwordHasher->hashPassword($user ,$password);
+
+            $user->setEmail($email);
+            $user->setPassword($hashpassword);
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', ' Compte crée avec succès');
+            return $this->redirectToRoute('app_login');
+        }
+        // Si le formulaire n'est pas encore soumis ou n'est pas valide, affichez la page avec le formulaire
+        return $this->render('security/new_user.html.twig');
+    }
 
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+
 }
