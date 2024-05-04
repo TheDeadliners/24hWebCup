5f00f08c030fa809a7693a6554c5b54c126625a7
AllanDev97 <134329913+AllanDev97@users.noreply.github.com>
2024-05-05 00:50:19 +0400

    Allan (#6)
    * feat/register
    
    * Fix:register
    
    * Fix:Mise a jour login
    
    * Fix:Ajout de champs dans le formulaire
    
    * Fix: restyle login,register
    
    * Fix : Couleurs Ok
    
    * Fix:Modification et optimisation pages

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
+import 'flowbite';
+
 import { Trade } from "./pages/Trade"
 import "./styles/app.css"
 import 'flowbite';
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,11 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -30,7 +26,7 @@ class SecurityController extends AbstractController
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
-    #[Route(path: '/creer-un-compte', name: 'app_user_new')]
+    #[Route(path: '/inscription', name: 'app_user_new')]
     public function newUser(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
     {
         // Vérifier si le formulaire a été soumis et que les champs requis sont remplis
@@ -49,14 +45,18 @@ class SecurityController extends AbstractController
                 return $this->redirectToRoute('app_user_new');
             }
             $user = new User();
+            $lastName = $request->request->get('lastname');
+            $firstName = $request->request->get('firstname');
+            $birthdate = $request->request->get('birthdate');
             $email = $request->request->get('email');
             $password = $request->request->get('password');
             $hashpassword = $passwordHasher->hashPassword($user ,$password);
 
             $user->setEmail($email);
+            $user->setLastName($lastName);
+            $user->setFirstName($firstName);
+            $user->setFirstName($birthdate);
             $user->setPassword($hashpassword);
-            $user->setFirstname($request->request->get('firstname'));
-            $user->setLastname($request->request->get('lastname'));
             $entityManager->persist($user);
             $entityManager->flush();
 
@@ -64,7 +64,7 @@ class SecurityController extends AbstractController
             return $this->redirectToRoute('app_login');
         }
         // Si le formulaire n'est pas encore soumis ou n'est pas valide, affichez la page avec le formulaire
-        return $this->render('security/new_user.html.twig');
+        return $this->render('security/register.html.twig');
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
