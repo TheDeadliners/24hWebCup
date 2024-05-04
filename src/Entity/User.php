1a99092b312d5041f40d1d7ac08123f7e3ef74e1
Thomas Epicharme <tepicharme.dev@protonmail.com>
2024-05-04 18:04:34 +0400

    feat: dashboard user add power and edit account

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,9 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -31,6 +34,29 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     #[ORM\Column]
     private ?string $password = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $firstname = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $lastname = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $birthdate = null;
+
+    /**
+     * @var Collection<int, SuperPower>
+     */
+    #[ORM\OneToMany(targetEntity: SuperPower::class, mappedBy: 'owner')]
+    private Collection $superPowers;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $profilePicture = null;
+
+    public function __construct()
+    {
+        $this->superPowers = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -105,5 +131,83 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): static
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): static
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getBirthdate(): ?\DateTimeInterface
+    {
+        return $this->birthdate;
+    }
+
+    public function setBirthdate(\DateTimeInterface $birthdate): static
+    {
+        $this->birthdate = $birthdate;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, SuperPower>
+     */
+    public function getSuperPowers(): Collection
+    {
+        return $this->superPowers;
+    }
+
+    public function addSuperPower(SuperPower $superPower): static
+    {
+        if (!$this->superPowers->contains($superPower)) {
+            $this->superPowers->add($superPower);
+            $superPower->setOwner($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSuperPower(SuperPower $superPower): static
+    {
+        if ($this->superPowers->removeElement($superPower)) {
+            // set the owning side to null (unless already changed)
+            if ($superPower->getOwner() === $this) {
+                $superPower->setOwner(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getProfilePicture(): ?string
+    {
+        return $this->profilePicture;
+    }
+
+    public function setProfilePicture(?string $profilePicture): static
+    {
+        $this->profilePicture = $profilePicture;
+
+        return $this;
     }
 }
