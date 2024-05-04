c4d6b5a9d291d858257a49910082749b205d3ca8
Thomas Epicharme <tepicharme.dev@protonmail.com>
2024-05-05 02:30:02 +0400

    env

---
+++ src/Controller/RestController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('ROLE_USER')]
+class RestController extends AbstractController
+{
+    #[Route('/trade', name: 'app_trade')]
+    public function index(): Response
+    {
+        return $this->render('trade/index.html.twig', [
+            'controller_name' => 'TradeController',
+        ]);
+    }
+}
