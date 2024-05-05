83140d0088e7da99c760b45da990706ccb1bbc6e
Thomas Epicharme <tepicharme.dev@protonmail.com>
2024-05-05 11:10:40 +0400

    is grandted

--- src/Controller/ConversationController.php
+++ src/Controller/ConversationController.php
@@ -12,8 +12,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Validator\Constraints\Timezone;
 
+#[IsGranted('ROLE_USER')]
 class ConversationController extends AbstractController
 {
     #[Route('/conversation', name: 'app_conversation')]
